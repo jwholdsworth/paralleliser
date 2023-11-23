43c918f2715db732276412dd01aa244d668d0849
James Holdsworth <jwholdsworth@gmail.com>
2023-11-23 21:15:03 +0000

    Making it work

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/jwholdsworth/paralleliser
+
+go 1.21.4
--- main.go
+++ main.go
@@ -1,22 +1,51 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"log"
 	"os"
+	"os/exec"
+	"sync"
 )
 
+var wg = sync.WaitGroup{}
+
 func main() {
+	command := os.Getenv("COMMAND")
+	if command == "" {
+		log.Fatal("COMMAND environment variable not specified")
+	}
+
 	entries, err := os.ReadDir("./")
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	for _, e := range entries {
-		if !e.IsDir() {
+	for _, entry := range entries {
+		if !entry.IsDir() {
 			continue
 		}
-		fmt.Println(e)
+		log.Println("Running in folder", entry.Name())
+		wg.Add(1)
+		go runCommandInFolder(command, entry.Name())
 	}
+	wg.Wait()
+}
+
+func runCommandInFolder(command string, folder string) {
+	var out bytes.Buffer
+
+	cmd := exec.Command("/bin/sh", "-c", command)
+	cmd.Stdout = &out
+	cmd.Dir = fmt.Sprintf("./%s", folder)
+	err := cmd.Run()
+	if err != nil {
+		log.Panic(err)
+	}
+
+	fmt.Println("Output from", folder)
+	fmt.Println(out.String())
+	wg.Done()
 }
